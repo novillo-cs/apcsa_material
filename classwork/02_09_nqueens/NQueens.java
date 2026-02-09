315d376c16d5310c577a21e01696b7a9a7b89dfd
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2026-02-09 08:35:19 -0500

    Create NQueens.java

---
+++ classwork/02_09_nqueens/NQueens.java
@@ -0,0 +1,27 @@
+public class NQueens{
+  private int[][]board;
+
+  /*Initialize board with a size by size 2D array. All values should be 0*/
+  public NQueens(int size){
+
+  }
+  
+
+  /**
+  *@return true when the queen added correctly, false Otherwise
+  *@precondition r and c are valid indices of the board array
+  *@postcondition the board is only changed when the function returns true
+  * in which case the queen is added and all it's threatened positions are incremented
+  */
+  private boolean addQueen(int r, int c){
+    return false;
+  }
+
+  /**Remove the queen that was added to r,c
+  *@precondition r and c are valid indices of the board array and there is a queen at position r,c
+  *@postcondition the board is modified to remove that queen and all it's
+  *threatened positions are decremented
+  */
+  private void removeQueen(int r, int c){
+
+  }
